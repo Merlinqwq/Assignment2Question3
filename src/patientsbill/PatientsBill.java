0b199af05a2d894565a7c45c277b7feb05e86200
Merlin <li-y92@142.132.137.142>
2018-11-29 11:51:08 -0600

    new commit

--- src/patientsbill/PatientsBill.java
+++ src/patientsbill/PatientsBill.java
@@ -10,40 +10,52 @@ public class PatientsBill {
   
     public static void main(String[] args) throws IOException{
         String strin;
+        int TotalBill=0;
+        int RoomBill=0;
+        int NurseBill=0;
+        int TVBill=0;
+        int PhoneBill=0;
+        final int RoomPriceI=395;
+        final int RoomPriceD=310;
+        final int RoomPriceP=350;
+        final int NursePriceS=250;
+        final int NursePriceN=275;
+        final int TVPrice=40;
+        final int PhonePrice=15;
         File data=new File("data.txt");
         File outcome=new File("outcome.txt");
         BufferedReader BR=new BufferedReader(new FileReader("data.txt"));
         FileWriter FW=new FileWriter("outcome.txt");
         PrintWriter PW=new PrintWriter(FW);
         strin=BR.readLine();
-        PW.print("PatientID   Days   Room   Nurse   TV   Phone    Bill");
+        PW.println("PatientID   Days   Room   Nurse   TV   Phone    Bill");
         while(strin!=null){
             String[] tokens=strin.split("[ ]+");
             int patientID=Integer.parseInt(tokens[0]);
             char RoomType=tokens[1].charAt(0);
             int Days=Integer.parseInt(tokens[2]);
             char Nurse=tokens[3].charAt(0);
-            char TVandPhone=tokens[4].charAt(0);
-            int TotalBill=0;
-            int RoomBill=0;
-            int NurseBill=0;
-            int TVBill=0;
-            int PhoneBill=0;
-            if(RoomType=='I'){RoomBill=Days*395;}
-            if(RoomType=='D'){RoomBill=Days*310;}
-            if(RoomType=='P'){RoomBill=Days*350;}
-            if(Nurse=='X'){NurseBill=0;}
-            if(Nurse=='S'){NurseBill=Days*250;}
-            if(Nurse=='N'){NurseBill=Days*275;}
-            if(TVandPhone=='V'){TVBill=Days*40;
-            PhoneBill=0;}
-            if(TVandPhone=='T'){PhoneBill=Days*15;
-            TVBill=0;}
-            if(TVandPhone=='X'){TVBill=0;
-            PhoneBill=0;}
+            char TV=tokens[4].charAt(0);
+            char Phone=tokens[4].charAt(0);
+            if(RoomType=='I'){
+                RoomBill=Days*RoomPriceI;
+                PhoneBill=0;
+                TVBill=0;
+                if(Nurse=='S'){ NurseBill=Days*NursePriceS; }
+                else if(Nurse=='N'){NurseBill=Days*NursePriceN;}
+            }
+            else{
+            if(RoomType=='P'){RoomBill=Days*RoomPriceP;}
+            if(RoomType=='D'){RoomBill=Days*RoomPriceD;}
+            NurseBill=0;
+            if(Phone=='T'){PhoneBill=Days*PhonePrice;}
+            else{PhoneBill=0;}
+            if(TV=='V'){TVBill=Days*TVPrice;}
+            else{TVBill=0;}
+            }
             TotalBill=RoomBill+NurseBill+TVBill+PhoneBill;
             strin=BR.readLine();
-            PW.printf("%d\t    %d\t   %d\t  %d\t  %d\t%d\t%d",patientID,Days,RoomBill,NurseBill,TVBill,PhoneBill,TotalBill);
+            PW.printf("%d\t    %d\t   %d\t  %d\t  %d\t%d\t%d\n",patientID,Days,RoomBill,NurseBill,TVBill,PhoneBill,TotalBill);
         }
         PW.close();
         System.out.println("check outcome in the outcome file");
